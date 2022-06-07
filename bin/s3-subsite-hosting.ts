c651f7dce541594ca6f67e29350b18e71fba43d4
Krzysztof Wielgo <krzysztof.wielgo@fortiumtech.com>
2022-06-07 01:20:38 +0100

    Format with prettier

--- bin/s3-subsite-hosting.ts
+++ bin/s3-subsite-hosting.ts
@@ -1,26 +1,33 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { SubsiteDeployments } from '../lib/subsite-deployments';
-import { Stack } from 'aws-cdk-lib';
-import { Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { strict as assert } from "assert";
+import "source-map-support/register";
+import { App, Stack } from "aws-cdk-lib";
+import { Source } from "aws-cdk-lib/aws-s3-deployment";
+import { SubsiteDeployments } from "../lib/subsite-deployments";
 
 const INDEX_DOC = "index.html";
 
-const app = new cdk.App();
+const app = new App();
 
 const subsiteDeploymentStack = new Stack(app, "SubsiteDeploymentsStack");
 
 const prefixInput = subsiteDeploymentStack.node.tryGetContext("subsite-prefix");
-const sourceDir = subsiteDeploymentStack.node.tryGetContext("subsite-dir-source");
+const sourceDir =
+  subsiteDeploymentStack.node.tryGetContext("subsite-dir-source");
 
-assert(prefixInput?.length > 0, "The prefix needs to be specified: 'subsite-prefix'");
-assert(sourceDir?.length > 0, "The source directory needs to be specified: 'subsite-dir-source'");
+assert(
+  prefixInput?.length > 0,
+  "The prefix needs to be specified: 'subsite-prefix'"
+);
+assert(
+  sourceDir?.length > 0,
+  "The source directory needs to be specified: 'subsite-dir-source'"
+);
 
-new SubsiteDeployments(subsiteDeploymentStack, 'SubsitesDeployments', {
+new SubsiteDeployments(subsiteDeploymentStack, "SubsitesDeployments", {
   indexDocument: INDEX_DOC,
   // Prefix to the subsite within the bucket
   prefix: prefixInput,
   // Subsite directory source to be uploaded
-  sources: [Source.asset(sourceDir)]
+  sources: [Source.asset(sourceDir)],
 });
--- lib/subsite-deployments.ts
+++ lib/subsite-deployments.ts
@@ -1,33 +1,36 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct } from 'constructs';
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 interface SubsiteDeploymentsProps {
-  indexDocument: string,
-  sources: [s3deploy.ISource],
-  prefix: string
-};
+  indexDocument: string;
+  sources: [s3deploy.ISource];
+  prefix: string;
+}
 
 export class SubsiteDeployments extends Construct {
-  readonly bucket: s3.Bucket
+  readonly bucket: s3.Bucket;
 
   constructor(scope: Construct, id: string, props: SubsiteDeploymentsProps) {
     super(scope, id);
 
-    this.bucket = new s3.Bucket(this, 'SubsiteDeploymentBucket', {
+    this.bucket = new s3.Bucket(this, "SubsiteDeploymentBucket", {
       websiteIndexDocument: props.indexDocument,
       publicReadAccess: true,
       // removalPolicy: RemovalPolicy.DESTROY,
       // autoDeleteObjects: true
     });
 
-    const deployment = new s3deploy.BucketDeployment(this, 'SubsiteDeployment', {
-      sources: props.sources,
-      destinationBucket: this.bucket,
-      // destinationKeyPrefix: props.prefix, // optional prefix in destination bucket
-    });
-
+    const deployment = new s3deploy.BucketDeployment(
+      this,
+      "SubsiteDeployment",
+      {
+        sources: props.sources,
+        destinationBucket: this.bucket,
+        // destinationKeyPrefix: props.prefix, // optional prefix in destination bucket
+      }
+    );
   }
 }
