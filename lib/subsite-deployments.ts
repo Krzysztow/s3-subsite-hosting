a2b405d02f470c1690447ee3900c310fb5414212
Krzysztof Wielgo <krzysztof.wielgo@fortiumtech.com>
2022-06-13 04:54:19 +0100

    Added deployment of the index generation lambda function

--- lib/subsite-deployments.ts
+++ lib/subsite-deployments.ts
@@ -1,7 +1,18 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import {
+  CfnOutput,
+  Duration,
+  lambda_layer_awscli,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import * as path from "path";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 const INDEX_DOC = "index.html";
@@ -21,11 +32,12 @@ export class SubsiteDeployments extends Construct {
       publicReadAccess: true,
       removalPolicy: RemovalPolicy.DESTROY, //TODO: depending on the environment
       autoDeleteObjects: true, //TODO: depending on the environment
-      lifecycleRules: [ //TODO: depending on environment - we don't want that for a production
+      lifecycleRules: [
+        //TODO: depending on environment - we don't want that for a production
         {
           prefix: `!${INDEX_DOC}`, //TODO: verify if that even works - this is prefix, I used file name instead
           expiration: Duration.days(1),
-        }
+        },
       ],
     });
 
@@ -38,5 +50,30 @@ export class SubsiteDeployments extends Construct {
         destinationKeyPrefix: props.prefix, // optional prefix in destination bucket
       }
     );
+
+    const updateIndexLambda = new lambdaNode.NodejsFunction(
+      this,
+      "CreateIndexFileLambda",
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "handler",
+        entry: path.join(__dirname, "../lib/lambda/create-index.ts"),
+        description:
+          "Scans bucket prefixes and creates root index.html with links to all index.html pages within prefixes",
+        logRetention: RetentionDays.ONE_DAY,
+        environment: {
+          BUCKET_NAME: this.bucket.bucketName,
+        },
+        bundling: {
+          nodeModules: [
+            '@aws-sdk/client-s3',
+          ],
+        },
+      }
+    );
+
+    this.bucket.grantReadWrite(updateIndexLambda);
+
+    new CfnOutput(this, "bucket-url", { value: this.bucket.urlForObject() });
   }
 }
